040ef8055615d19d667e1ce38d4bebefbf4bccd3
Eric Claflin <ericclaflin@Erics-MacBook-Pro.local>
2026-09-20 10:33:36 -0400

    revert decision to parse json in the int layer
    Recognizing a need to coalesce the amenity json with the listing amenity
    snapshot to safeguard potential gaps, pulling the json parse to boolean
    flag approach downstream into the mart layer to prevent repeating the
    same function in different places

--- models/int/int_amenities__effective_ranges.sql
+++ models/int/int_amenities__effective_ranges.sql
@@ -30,29 +30,7 @@ effective_ranges as (
             order by change_at
         ) as effective_to
     from deduped
-),
-
-/*
-    Making a design decision to parse for specifically relevant amenities here.  This will both
-    prevent having to do parsing in the mart layer as well as simplify join structure to this
-    scd2 table in the mart layer.  The alternative approach would be to all possible
-    values fan the grain of this table out to be one row per listing *and ammenity* for a given
-    effective window.  Again, this makes the join structure more complicated downstream in the mart layer.
-
-    Another option could be looping through all possible amenity values to create a wide table
-    of boolean values for each possible amenity, but this has production downsides w/r/t schema instability
-*/
-parsed_amenities as (
-    select
-        *,
-        -- "Lockbox" "First aid kit"
-        (amenities::jsonb) @> '["Air conditioning"]'::jsonb as has_air_conditioning,
-        (amenities::jsonb) @> '["Lockbox"]'::jsonb as has_lockbox,
-        (amenities::jsonb) @> '["First aid kit"]'::jsonb as has_first_aid_kit
-    from
-        effective_ranges
 )
 
 select *
-from parsed_amenities
-
+from effective_ranges
